f23b27390241764c659fc965bcc0caeb610d5583
joseberea <jose.berea@gmail.com>
2015-11-12 15:49:21 +0000

    Initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+    # List submit
+    def index
+        @products = Product.all
+    end
+    
+    # Show submit
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    # Add handler
+    def new
+    end
+    
+    # Add submit
+    def create
+        @product = Product.new(product_params)
+        @product.save
+        #redirect_to @product
+        #redirect_to product_path(id: @product.id)
+        redirect_to products_path
+    end
+    
+    # Edit handler
+    def edit
+        @product = Product.find(params[:id])
+    end
+    
+    # Edit submit
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to products_path
+        else    
+            render 'edit'
+        end
+    end
+    
+    # Delete submit
+    def destroy
+        redirect_to new_product_path
+    end
+    
+    # PROVATE METHODS
+    private
+      def product_params
+        params.require(:product).permit(:name, :description)
+      end    
+end
